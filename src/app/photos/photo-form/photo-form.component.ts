c31c751b8755a6805c14b98eccf9d3ad0611239c
Hércules Pedrosa Lemos Junior <hercules.lemos@lais.huol.ufrn.br>
2019-03-12 08:45:02 -0300

    first commit

---
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'ap-photo-form',
+  templateUrl: './photo-form.component.html',
+  styleUrls: ['./photo-form.component.css']
+})
+export class PhotoFormComponent implements OnInit {
+
+
+  photoForm: FormGroup;
+  file: File;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required ],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true]
+    })
+
+  }
+
+  upload() {
+
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+  }
+
+}
